483d998ac12dc7741a0d8b9dee844cea26bbff01
thananan27 <thananan27@gmail.com>
2021-07-28 22:35:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?PHP
-$data = https://covid19.th-stat.com/json/covid19v2/getTodayCases.json;
-header('Content-Type: application/json');
-echo json_encode($data);
-?>
+$ch = curl_init();
+// IMPORTANT: the below line is a security risk, read https://paragonie.com/blog/2017/10/certainty-automated-cacert-pem-management-for-php-software
+// in most cases, you should set it to true
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'https://covid19.th-stat.com/json/covid19v2/getTodayCases.json');
+$result = curl_exec($ch);
+curl_close($ch);
+
+$obj = json_decode($result);
+echo $obj->access_token;
