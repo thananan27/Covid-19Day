33b546f0f8a4746c92d1e5195b8117e97a116b5f
thananan27 <thananan27@gmail.com>
2021-07-29 00:41:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,5 @@
 	$Hospitalized = $json["Hospitalized"];
 	$Recovered = $json["Recovered"];
 	
-echo Update ล่าสุดเมื่อ : $UpdateDate ติดเชื้อสะสม : $Confirmed ราย ติดเชื้อเพิ่มขึ้น : $NewConfirmed ราย เสียชีวิตสะสม : $Deaths ราย เสียชีวิตเพิ่มขึ้น : $NewDeaths ราย รักษาตัวในรพ.ตอนนี้ : $Hospitalized ราย  รักษาตัวในรพ.เพิ่มขึ้น : $NewHospitalized ราย  หายแล้วตอนนี้ : $Recovered ราย  หายแล้วเพิ่มขึ้น : $NewHospitalized ราย ;
+echo "Update ล่าสุดเมื่อ : "$UpdateDate "ติดเชื้อสะสม : "$Confirmed "ราย ติดเชื้อเพิ่มขึ้น : "$NewConfirmed "ราย เสียชีวิตสะสม : "$Deaths "ราย เสียชีวิตเพิ่มขึ้น  : "$NewDeaths "ราย รักษาตัวในรพ.ตอนนี้ : "$Hospitalized "ราย  รักษาตัวในรพ.เพิ่มขึ้น : "$NewHospitalized "ราย  หายแล้วตอนนี้ : "$Recovered "ราย  หายแล้วเพิ่มขึ้น : "$NewHospitalized "ราย ";
 ?> 
