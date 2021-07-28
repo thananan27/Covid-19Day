6a7a1a4f1fcc343023301b3b949ede2cc3dc6642
thananan27 <thananan27@gmail.com>
2021-07-29 00:46:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
     $url = "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";
     $json = json_decode(file_get_contents($url), true);
 	$UpdateDate = $json["UpdateDate"];
-	$Confirmed = $json["Confirmed"];
-	$NewConfirmed = $json["NewConfirmed"];
-	$Deaths = $json["Deaths"];
-	$Hospitalized = $json["Hospitalized"];
-	$Recovered = $json["Recovered"];
+
 	
 echo $UpdateDate;
 ?> 
