0239cb3696543c21fa1a89e2f4c0bf3751404a24
thananan27 <thananan27@gmail.com>
2021-07-28 22:53:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
-$json_url = "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";  
-$crl = curl_init();
-curl_setopt($crl, CURLOPT_URL, $json_url);
-curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($crl, CURLOPT_SSL_VERIFYPEER, FALSE); 
-$json = curl_exec($crl);
-curl_close($crl);
-$emp = json_decode($json, TRUE);
-?>
+
+
+$testUrl = 'https://covid19.th-stat.com/json/covid19v2/getTodayCases.json';
+
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $testUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);// localhost, development only.
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);// localhost, development only.
+
+$output = curl_exec($ch);
+if ($output === false) {
+    // if curl error.
+    $curlError = curl_error($ch);
+    $curlErrorNo = curl_errno($ch);
+}
+
+curl_close($ch);
+
+
+echo 'curl result:<br>' . PHP_EOL;
+var_dump($output);
+
+if (isset($curlError)) {
+    echo 'Error:<br>' . PHP_EOL;
+    var_dump($curlError);
+    echo 'Error code:<br>' . PHP_EOL;
+    var_dump($curlErrorNo);
+}
