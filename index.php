49a61fa788d90964fa9f7f45999e0dbd7d6669cb
thananan27 <thananan27@gmail.com>
2021-07-29 00:51:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     $url = "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";
     $json = json_decode(file_get_contents($url), true);
-	$UpdateDate = $json["UpdateDate"];
+	$Date = $json["UpdateDate"];
 
 	
-echo $UpdateDate;
+echo $Date;
 ?> 
