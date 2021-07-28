6355cc2af10d2bbfc5f9be50cf615e8d796d587d
thananan27 <thananan27@gmail.com>
2021-07-28 23:24:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ $url = "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";
 //call api
 $json = file_get_contents($url);
 $json = json_decode($json);
-$lat = $json->results[0]->UpdateDate;
-$lng = $json->results[0]->Confirmed;
-echo "Update ล่าสุดเมื่อ: " . $UpdateDate . ", ติดเชื้อสะสม: " . $Confirmed;
+$date = $json->results[0]->UpdateDate;
+$Confirm = $json->results[0]->Confirmed;
+echo "Update ล่าสุดเมื่อ: " . $Date . ", ติดเชื้อสะสม: " . $Confirm;
 
 // output
 // Latitude: 40.6781784, Longitude: -73.9441579
