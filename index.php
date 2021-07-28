a16ca0a2c479b0107868540a157cd0c44eb5c867
thananan27 <thananan27@gmail.com>
2021-07-29 00:17:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
     $url = "https://dogechain.info/api/v1/address/balance/DMr3fEiVrPWFpoCWS958zNtqgnFb7QWn9D";
     $json = json_decode(file_get_contents($url), true);
-    $balance = json("balance")
-
-    echo $balance
+	$balance = $json["balance"];
+echo $balance;
 ?> 
