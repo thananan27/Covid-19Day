247cda911fee37a2a84780966236d0aca7144c1d
thananan27 <thananan27@gmail.com>
2021-07-29 00:35:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-    $url = "https://dogechain.info/api/v1/address/balance/DMr3fEiVrPWFpoCWS958zNtqgnFb7QWn9D";
+    $url = "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";
     $json = json_decode(file_get_contents($url), true);
-	$balance = $json["balance"];
-echo $balance;
+	$UpdateDate = $json["UpdateDate"];
+	$Confirmed = $json["Confirmed"];
+	$NewConfirmed = $json["NewConfirmed"];
+	$Deaths = $json["Deaths"];
+	$Hospitalized = $json["Hospitalized"];
+	$Recovered = $json["Recovered"];
+	
+echo Update ล่าสุดเมื่อ : $UpdateDate ติดเชื้อสะสม : $Confirmed ราย ติดเชื้อเพิ่มขึ้น : $NewConfirmed ราย เสียชีวิตสะสม : $Deaths ราย เสียชีวิตเพิ่มขึ้น : $NewDeaths ราย รักษาตัวในรพ.ตอนนี้ : $Hospitalized ราย  รักษาตัวในรพ.เพิ่มขึ้น : $NewHospitalized ราย  หายแล้วตอนนี้ : $Recovered ราย  หายแล้วเพิ่มขึ้น : $NewHospitalized ราย ;
 ?> 
