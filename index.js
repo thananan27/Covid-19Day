df88b3f0bc972694bc60313c7795a8c59ecddf46
thananan27 <thananan27@gmail.com>
2021-07-28 23:02:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var url = 'https://covid19.th-stat.com/json/covid19v2/getTodayCases.json';
-var jsonObject;
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-     jsonObject = JSON.parse(body);
-     console.log(jsonObject);
-  }
+const https = require('https');
+
+let url = "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";
+
+https.get(url,(res) => {
+    let body = "";
+
+    res.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    res.on("end", () => {
+        try {
+            let json = JSON.parse(body);
+            // do something with JSON
+        } catch (error) {
+            console.error(error.message);
+        };
+    });
+
+}).on("error", (error) => {
+    console.error(error.message);
 });
