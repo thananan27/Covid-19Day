79d0e34702728046708b5bc2a7a3ba93183590c8
thananan27 <thananan27@gmail.com>
2021-07-29 15:43:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-$json = file_get_contents('https://covid19.th-stat.com/json/covid19v2/getTodayCases.json');
-$obj = json_decode($json);
-echo $obj->Confirmed;
-?> 
+  $data_content =  file_get_contents("https://ddc.moph.go.th/covid19-dashboard/index.php?dashboard=province");
+  echo $data_content;
+?>
