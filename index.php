5f9292efb7724117a9391cf5e7e9adaa0b7b18d8
thananan27 <thananan27@gmail.com>
2021-07-29 11:03:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-$url = "https://covid19.th-stat.com/json/covid19v2/getTodayCases.json";
-$json = file_get_contents($url);
-$json_data = json_decode($json, true);
-echo "UpdateDate: ". $json_data["Confirmed"];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'https://covid19.th-stat.com/json/covid19v2/getTodayCases.json');
+$result = curl_exec($ch);
+curl_close($ch);
+
+$obj = json_decode($result);
+echo $obj->Confirmed;
+
 ?> 
